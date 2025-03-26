4bc2139a23f26484f5c8939fe24f3711b71e54c8
wiwells05 <wiwell@unc.edu>
2025-03-26 02:42:37 -0400

    first attempt ex03

--- exercises/ex03/dictionary.py
+++ exercises/ex03/dictionary.py
@@ -7,9 +7,12 @@ def invert(given_dict: dict[str, str]) -> dict[str, str]:
     """take a dictionary and return a dictionary with keys and values swapped"""
     inverse_dict: dict[str, str] = {}
     for key in given_dict:
-        if given_dict[key] in inverse_dict:
+        if given_dict[key] in inverse_dict:  # check to see if the key already exists
             raise KeyError("cannot have duplicate keys")
-        inverse_dict[given_dict[key]] = key
+        inverse_dict[given_dict[key]] = (
+            key  # the key here essentially evaluates to the original value and then
+            # assigns the previous key to the value
+        )
 
     return inverse_dict
 
@@ -18,23 +21,35 @@ def count(given_list: list[str]) -> dict[str, int]:
     """count the appearances of an item in a list and store results in a dictionary"""
     count_dict: dict[str, int] = {}
     for i in given_list:
-        if i in count_dict:
+        if (
+            i in count_dict
+        ):  # check if the str is already accounted for, if so, add to the count
             count_dict[i] += 1
         else:
-            count_dict[i] = 1
+            count_dict[i] = 1  # if not then create a new key with value 1
 
     return count_dict
 
 
 def favorite_color(given_colors: dict[str, str]) -> str:
     """returns the name of the favorite color that appears most frequently"""
-    color_list: list[str] = list(given_colors.values())
-    color_list.append("")
-    counted_dict: dict[str, int] = count(color_list)
-    popular_color: str = color_list[0]
+    color_list: list[str] = list(
+        given_colors.values()
+    )  # take a list of values from the dict so that it can go into the count function
+    color_list.append(
+        ""
+    )  # append empty list so in the case of an empty list, the code will still execute
+    counted_dict: dict[str, int] = count(color_list)  # use the count function
+    popular_color: str = color_list[
+        0
+    ]  # give the function a baseline color to compare to later
     for key in counted_dict:
-        if counted_dict[key] > counted_dict[popular_color]:
-            popular_color = key
+        if (
+            counted_dict[key] > counted_dict[popular_color]
+        ):  # check if the count of the key is greater than the first encountered key
+            # or popular color. this also naturally breaks ties by order of appearance
+            popular_color = key  # if the count is higher for a key, that key then
+            # becomes the new popular color
 
     return popular_color
 
@@ -44,8 +59,11 @@ def bin_len(word_list: list[str]) -> dict[int, set[str]]:
     len_dict: dict[int, set[str]] = {}
     for i in word_list:
         if len(i) in len_dict:
-            len_dict[len(i)].add(i)
-        else:
-            len_dict[len(i)] = set()
-            len_dict[len(i)].add(i)
+            len_dict[len(i)].add(
+                i
+            )  # if an integer key for the length of the word already exists, then add
+            # the word to the set for that key
+        else:  # if it doesn't already exist,
+            len_dict[len(i)] = set()  # make an empty set
+            len_dict[len(i)].add(i)  # and add the word to it
     return len_dict
--- exercises/ex03/dictionary_test.py
+++ exercises/ex03/dictionary_test.py
@@ -1,3 +1,142 @@
 """exercise 03 - Dictionary Tests"""
 
 __author__: str = "730774516"
+
+
+from exercises.ex03.dictionary import invert, count, favorite_color, bin_len
+
+
+def test_invert_use_1() -> None:
+    """test the invert function from dictionary.py with a use case"""
+    test_dict: dict[str, str] = {
+        "A": "1",
+        "B": "2",
+        "C": "3",
+    }  # testing a normal use of the function
+    assert invert(test_dict) == {
+        "1": "A",
+        "2": "B",
+        "3": "C",
+    }, print("ERROR: output did not match expected output")
+
+
+def test_invert_use_2() -> None:
+    """test the invert function from dictionary.py with a use case"""
+    test_dict: dict[str, str] = {
+        "A": "3",
+        "B": "2",
+        "C": "1",
+    }  # testing another normal use of the function
+    assert invert(test_dict) == {
+        "3": "A",
+        "2": "B",
+        "1": "C",
+    }, print("ERROR: output did not match expected output")
+
+
+def test_invert_edge() -> None:
+    """test the invert function from dictionary.py with an edge case (empty dict)"""
+    test_dict: dict[str, str] = (
+        {}
+    )  # testing the function when given an empty dictionary
+    assert invert(test_dict) == {}, print("ERROR: output did not match expected output")
+
+
+def test_count_use_1() -> None:
+    """test the count function from dictionary.py with a use case"""
+    test_list: list[str] = [
+        "A",
+        "B",
+        "C",
+        "C",
+    ]  # testing the function with a very standard list
+    assert count(test_list) == {
+        "A": 1,
+        "B": 1,
+        "C": 2,
+    }, print("ERROR: output did not match expected output")
+
+
+def test_count_use_2() -> None:
+    """test the count function from dictionary.py with a use case"""
+    test_list: list[str] = [
+        "A",
+        "A",
+        "A",
+        "A",
+    ]  # testing with another standard list but this time all values are the same
+    assert count(test_list) == {"A": 4}, print(
+        "ERROR: output did not match expected output"
+    )
+
+
+def test_count_edge() -> None:
+    """test the count function from dictionary.py with an edge case"""
+    test_list: list[str] = []  # testing with an empty list
+    assert count(test_list) == {}, print("ERROR: output did not match expected output")
+
+
+def test_favorite_color_use_1() -> None:
+    """test the favorite color function from dictionary.py with a use case"""
+    test_dict: dict[str, str] = {
+        "Amy": "Green",
+        "John": "Blue",
+        "Carter": "Green",
+    }  # testing with a standard dictionary
+    assert favorite_color(test_dict) == "Green", print(
+        "ERROR: output did not match expected output"
+    )
+
+
+def test_favorite_color_use_2() -> None:
+    """test the favorite color function from dictionary.py with a use case"""
+    test_dict: dict[str, str] = {
+        "Amy": "Green",
+        "John": "Blue",
+        "Carter": "Red",
+    }  # testing that the first encountered color wins in the case of a tie
+    assert favorite_color(test_dict) == "Green", print(
+        "ERROR: output did not match expected output"
+    )
+
+
+def test_favorite_color_edge() -> None:
+    """test the favorite color function from dictionary.py with an edge case"""
+    test_dict: dict[str, str] = {}  # testing with an empty dictionary
+    assert favorite_color(test_dict) == "", print(
+        "ERROR: output did not match expected output"
+    )
+
+
+def test_bin_len_use_1() -> None:
+    """test the bin_len function from dictionary.py with a use case"""
+    test_list: list[str] = [
+        "The",
+        "Big",
+        "Dog",
+        "Jumps",
+    ]  # testing with a standard list
+    assert bin_len(test_list) == {3: {"The", "Big", "Dog"}, 5: {"Jumps"}}, print(
+        "ERROR: output did not match expected output"
+    )
+
+
+def test_bin_len_use_2() -> None:
+    """test the bin_len function from dictionary.py with a use case"""
+    test_list: list[str] = [
+        "The",
+        "Big",
+        "Dog",
+        "Dog",
+    ]  # testing with a standard list but all words are of equal length
+    assert bin_len(test_list) == {3: {"The", "Big", "Dog", "Dog"}}, print(
+        "ERROR: output did not match expected output"
+    )
+
+
+def test_bin_len_edge() -> None:
+    """test the bin_len function from dictionary.py with an edge case"""
+    test_list: list[str] = []  # testing with an empty list
+    assert bin_len(test_list) == {}, print(
+        "ERROR: output did not match expected output"
+    )
